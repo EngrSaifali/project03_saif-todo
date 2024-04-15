b1aef24c82be7efb37fe79458f039121592c3668
EngrSaifali <saifaliapril2004@gmail.com>
2024-04-15 21:29:16 +0500

    To-do List using Typescript

---
+++ main.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+// To-Do List using Typescript
+import inquirer from "inquirer";
+let taskManager = [];
+let condition = true;
+while (condition) {
+    let todos = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "What task would you like to add?"
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "Want to add more tasks?",
+            default: "true"
+        }
+    ]);
+    taskManager.push(todos.firstQuestion);
+    console.log(taskManager);
+    condition = todos.secondQuestion;
+}
